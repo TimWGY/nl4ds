b2a03a28cb189fe9049f985c4c0ff1dfcb6a819f
Tim Wu <gw923@nyu.edu>
2021-12-19 12:15:01 -0500

    Add resize and make_greyscale functionality

--- ocr_functions.py
+++ ocr_functions.py
@@ -9,6 +9,7 @@ import os
 from glob import glob
 import re
 from collections import Counter
+import ast
 
 import time
 from PIL import Image
@@ -83,7 +84,7 @@ def mark_ms_ocr_result(image_file_path, components_df, fontsize=10, filename='')
 
   image = Image.open(image_file_path)
 
-  plt.figure(figsize=(20, 20), dpi=300)
+  plt.figure(figsize=(20, 20), dpi=150)
   ax = plt.imshow(image, cmap=cm.gray)
 
   polygons = []
@@ -96,7 +97,77 @@ def mark_ms_ocr_result(image_file_path, components_df, fontsize=10, filename='')
     ax.axes.add_patch(patch)
     plt.text(vertices[1][0], vertices[1][1], ocr_text, fontsize=fontsize, color='r', va="top")
 
-  plt.show()
-
   if filename != '':
     plt.savefig(filename + '.png', bbox_inches='tight', pad_inches=0)
+
+
+######################################################################################
+
+import cv2
+
+
+def make_greyscale_img(img_path):
+  out_img_path = img_path.replace('.', '_grey.')
+  if not os.path.exists(out_img_path):
+    grey_img = cv2.imread(img_path, 0)
+    im = Image.fromarray(grey_img)
+    im.save(out_img_path)
+    return out_img_path
+  else:
+    print('Greyscale version of the image already exists.')
+    return out_img_path
+
+
+def resize_img(img_path, target_size):
+  img = cv2.imread(img_path)
+  img_shape = img.shape
+  if img_shape[0] != img_shape[1]:
+    raise '[Error] Input image not a square shape.'
+  orig_size = img_shape[0]
+  out_img_path = img_path.replace('.', '_' + str(orig_size) + '_to_' + str(target_size) + '.')
+  out_img_path = re.sub(r'(000)(_|\.)', r'k\2', out_img_path)
+  if not os.path.exists(out_img_path):
+    resized_img = cv2.resize(img, (target_size, target_size))
+    im = Image.fromarray(resized_img)
+    im.save(out_img_path)
+    return out_img_path
+  else:
+    print('Resized version of the image already exists.')
+    return out_img_path
+
+######################################################################################
+
+
+import json
+
+
+def save_dict_to_json(dic, filepath):
+  with open(filepath, 'w') as f:
+    json.dump(dic, f)
+
+
+def read_dict_from_json(filepath):
+  with open(filepath, 'r') as f:
+    dic = json.load(f)
+  return dic
+
+def ms_ocr(img_path):
+
+  raw_ocr_result_filepath = img_path.split('.')[0] + '_raw_ocr_result.txt'
+  if not os.path.exists(raw_ocr_result_filepath):
+    result = get_ms_ocr_result(img_path)
+    save_dict_to_json(result, raw_ocr_result_filepath)
+
+  ocr_result_table_filepath = img_path.split('.')[0] + '_ocr_result_table.csv'
+  if not os.path.exists(ocr_result_table_filepath):
+    comp_df = parse_ms_ocr_result(result)
+    comp_df.to_csv(ocr_result_table_filepath, index=False)
+  else:
+    print('OCR result table found.')
+    comp_df = pd.read_csv(ocr_result_table_filepath)
+    comp_df['bounding_box'] = comp_df['bounding_box'].apply(ast.literal_eval)
+
+  ocr_result_marked_img_path = img_path.split('.')[0] + '_ocr_result_marked_img.' + img_path.split('.')[1]
+  mark_ms_ocr_result(img_path, comp_df, fontsize=10, filename=ocr_result_marked_img_path)
+
+######################################################################################
